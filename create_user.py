e8a1e9fd06760699075cff3516b79eae849575d6
guitoebe <131931248+guitoebe@users.noreply.github.com>
2025-11-17 21:03:54 -0300

    Remove admin user creation logic
    Remove code for creating an admin user.

--- create_user.py
+++ create_user.py
@@ -1,10 +1,1 @@
-from src import create_app, db
-from src.models.admin_user import AdminUser
 
-app = create_app()
-with app.app_context():
-    u = AdminUser(username="admin")
-    u.set_password("1234")
-    db.session.add(u)
-    db.session.commit()
-    print("Admin user created with username 'admin' and password '1234'")
